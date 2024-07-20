609bd0770029311829f544ee532266a42f9dc499
Ashwin Naren <arihant2math@gmail.com>
2024-07-19 23:39:39 -0700

    Code that is written in C++ ... by I HATE C++. There's gotta be some bugs in this stuff

--- src/main/driver/cpp/driversource.cpp
+++ src/main/driver/cpp/driversource.cpp
@@ -1,1 +1,38 @@
-#include "driversource.h"
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include "kissnet.h"
+
+// Defaults
+#define DEFAULT_IP "10.40.14.11"
+#define DEFAULT_PORT 5800
+
+using namespace std::chrono_literals;
+namespace kn = kissnet;
+
+struct Pose {
+    float x;
+    float y;
+    float z;
+    float roll;
+    float pitch;
+    float yaw;
+};
+
+class PixelentClient {
+    private:
+        kn::udp_socket socket;
+    public:
+        PixelentClient(const std::string ip, const uint16_t port) : socket{kn::endpoint(ip, port)}  {
+        }
+
+        PixelentClient() : PixelentClient(DEFAULT_IP, DEFAULT_PORT) {
+        }
+
+        PixelentClient(const std::string ip) : PixelentClient(ip, DEFAULT_PORT) {
+        }
+
+        PixelentClient(const uint16_t port) : PixelentClient(DEFAULT_IP, port) {
+        }
+
+};
--- src/main/driver/include/driversource.h
+++
@@ -1,7 +0,0 @@
-#ifndef driversource_h
-#define driversource_h
-class PixelentClient {
-public:
-    udp_client;
-}
-#endif
